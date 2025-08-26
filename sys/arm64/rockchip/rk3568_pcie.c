175ace718c4cbc706a24dbafd8a22bbd609d98fb
MartinFx <maficccc@gmail.com>
2025-08-26 10:13:51 +0200

    bpi-r2-pro: test kernel definition

--- sys/arm64/rockchip/rk3568_pcie.c
+++ sys/arm64/rockchip/rk3568_pcie.c
@@ -292,7 +292,6 @@ rk3568_pcie_detach(device_t dev)
 static int
 rk3568_pcie_attach(device_t dev)
 {
-    panic("not implemented yet");
 	struct rk3568_pcie_softc *sc = device_get_softc(dev);
 	int error;
     device_printf(dev, "pcie attach: %d\n", __LINE__);
