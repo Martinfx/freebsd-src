a4fd92e3de8ecc2ea01a9f7cf24f583c25451610
Martin Filla <freebsd@sysctl.cz>
2025-12-17 15:45:56 +0100

    bananapi-r64: support for mediatek mt7622-pcie

--- sys/arm64/mediatek/mdtk_clk.h
+++ sys/arm64/mediatek/mdtk_clk.h
@@ -170,3 +170,11 @@ void mdtk_clkdev_device_unlock(device_t dev);
 void mdtk_register_clocks(device_t dev, struct mdtk_clk_def *cldef);
 int mdtk_hwreset_by_idx(struct mdtk_clk_softc *sc, intptr_t idx, bool reset);
 #endif
+
+
+
+
+
+
+
+// XXX Test workflow
