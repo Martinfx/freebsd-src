6901803e39915d4e4617649fb4c6a4e2b154116d
Martin Filla <freebsd@sysctl.cz>
2026-03-26 19:48:23 +0100

    WIP: TEST uart driver
    Signed-off-by: Martin Filla <freebsd@sysctl.cz>

--- sys/arm64/mediatek/mt7622_uart.c
+++ sys/arm64/mediatek/mt7622_uart.c
@@ -37,89 +37,206 @@
 #include <sys/kernel.h>
 #include <sys/module.h>
 #include <sys/sysctl.h>
+
 #include <machine/bus.h>
 
 #include <dev/clk/clk.h>
+#include <dev/ic/ns16550.h>
 #include <dev/ofw/ofw_bus.h>
 #include <dev/ofw/ofw_bus_subr.h>
 #include <dev/uart/uart.h>
+#include <dev/uart/uart_bus.h>
 #include <dev/uart/uart_cpu.h>
 #include <dev/uart/uart_cpu_fdt.h>
-#include <dev/uart/uart_bus.h>
 #include <dev/uart/uart_dev_ns8250.h>
-#include <dev/ic/ns16550.h>
 
 #include "uart_if.h"
+
+#define MT_UART_RATE_FIX     0x0d /* Rate fix register */
+#define MT_UART_HIGHS	     0x09 /* Highspeed mode register */
+#define MT_UART_SAMPLE_COUNT 0x0a /* Sample count register */
+#define MT_UART_SAMPLE_POINT 0x0b /* Sample point register */
+#define MT_UART_FRACDIV_L    0x15 /* Fractional divisor LSB */
+#define MT_UART_FRACDIV_M    0x16 /* Fractional divisor MSB */
+
+/*
+ * Fractional divisor lookup tables.
+ *
+ * Indexed by (fraction / 10), where:
+ *   fraction = ((rclk * 100) / baud / quot) % 100
+ *
+ * These come directly from the Linux mtk8250 driver and are used to
+ * fine-tune the baud rate accuracy in highspeed mode 3.
+ */
+static const uint8_t mt_fraction_L_mapping[] = { 0x00, 0x01, 0x05, 0x15, 0x55,
+	0x57, 0x57, 0x77, 0x7F, 0xFF, 0xFF };
+
+static const uint8_t mt_fraction_M_mapping[] = { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+	3 };
+
+/*
+ * TODO: Override low-level ops (.init) to support MTK highspeed
+ * mode 3 for console path. Currently the console uses standard
+ * ns8250 16x divisor, which works for baud rates that divide
+ * evenly from rclk (e.g. 25MHz/16/1 = 1562500).
+ *
+ * For arbitrary baud rates (115200, 921600, etc.), MTK highspeed
+ * mode 3 with sample_count/sample_point and fractional divisor
+ * is needed for accurate clocking. See Linux 8250_mtk.c.
+ *
+ */
 
 /*
  * High-level UART interface.
  */
-struct mdtk_softc {
-	struct ns8250_softc 	ns8250_base;
-	clk_t			clk;
+struct mt_softc {
+	struct ns8250_softc ns8250_base;
+	clk_t baud_clk;
+	clk_t bus_clk;
 };
 
-/*
- * UART class interface.
- */
 static int
-mdtk_uart_attach(struct uart_softc *sc)
+mt_uart_attach(struct uart_softc *sc)
 {
 	int rv;
-	struct ns8250_softc *ns8250 = (struct ns8250_softc*)sc;
+	struct ns8250_softc *ns8250 = (struct ns8250_softc *)sc;
 	struct uart_bas *bas = &sc->sc_bas;
 
 	rv = ns8250_bus_attach(sc);
 	if (rv != 0)
 		return (rv);
 
-	ns8250->ier_rxbits = 0x1d;
 	ns8250->ier_mask = 0xc0;
 	ns8250->ier = uart_getreg(bas, REG_IER) & ns8250->ier_mask;
 	ns8250->ier |= ns8250->ier_rxbits;
 	uart_setreg(bas, REG_IER, ns8250->ier);
 	uart_barrier(bas);
+
+	// Some MediaTek SoCs need this to enable correct clock dividing.
+	uart_setreg(bas, MT_UART_RATE_FIX, 0x00);
+	uart_barrier(bas);
+
 	return (0);
 }
 
-static void
-mdtk_uart_grab(struct uart_softc *sc)
+static int
+mt_bus_param(struct uart_softc *sc, int baudrate, int databits, int stopbits,
+    int parity)
 {
-	struct uart_bas *bas = &sc->sc_bas;
-	struct ns8250_softc *ns8250 = (struct ns8250_softc*)sc;
-	u_char ier;
+	struct uart_bas *bas;
+	uint8_t lcr;
 
-	/*
-	 * turn off all interrupts to enter polling mode. Leave the
-	 * saved mask alone. We'll restore whatever it was in ungrab.
-	 * All pending interrupt signals are reset when IER is set to 0.
-	 */
+	bas = &sc->sc_bas;
+	lcr = 0;
+
+	/* Data bits */
+	switch (databits) {
+	case 5:
+		lcr |= CFCR_5BITS;
+		break;
+	case 6:
+		lcr |= CFCR_6BITS;
+		break;
+	case 7:
+		lcr |= CFCR_7BITS;
+		break;
+	case 8:
+	default:
+		lcr |= CFCR_8BITS;
+		break;
+	}
+
+	/* Stop bits */
+	if (stopbits == 2)
+		lcr |= CFCR_STOPB;
+
+	/* Parity */
+	switch (parity) {
+	case UART_PARITY_EVEN:
+		lcr |= CFCR_PENAB | CFCR_PEVEN;
+		break;
+	case UART_PARITY_ODD:
+		lcr |= CFCR_PENAB;
+		break;
+	case UART_PARITY_MARK:
+		lcr |= CFCR_PENAB | CFCR_PONE;
+		break;
+	case UART_PARITY_SPACE:
+		lcr |= CFCR_PENAB | CFCR_PZERO;
+		break;
+	case UART_PARITY_NONE:
+	default:
+		break;
+	}
+
 	uart_lock(sc->sc_hwmtx);
-	ier = uart_getreg(bas, REG_IER);
-	uart_setreg(bas, REG_IER, ier & ns8250->ier_mask);
 
-	while ((uart_getreg(bas, REG_LSR) & LSR_TEMT) == 0)
-		;
-
-	uart_setreg(bas, REG_FCR, 0);
+	/* Set line control register */
+	uart_setreg(bas, REG_LCR, lcr);
 	uart_barrier(bas);
-	uart_unlock(sc->sc_hwmtx);
-}
 
-static void
-mdtk_uart_ungrab(struct uart_softc *sc)
-{
-	struct ns8250_softc *ns8250 = (struct ns8250_softc*)sc;
-	struct uart_bas *bas = &sc->sc_bas;
+	/* Set baud rate with MTK-specific logic */
+	if (baudrate > 0 && bas->rclk > 0) {
+		uint32_t quot, fraction, tmp;
 
-	/*
-	 * Restore previous interrupt mask
-	 */
-	uart_lock(sc->sc_hwmtx);
-	uart_setreg(bas, REG_FCR, ns8250->fcr);
-	uart_setreg(bas, REG_IER, ns8250->ier);
-	uart_barrier(bas);
+		if (baudrate < 115200) {
+			/* Standard mode: 16x oversampling */
+			uart_setreg(bas, MT_UART_HIGHS, 0x0);
+			uart_barrier(bas);
+
+			quot = (bas->rclk + 8 * baudrate) / (16 * baudrate);
+			if (quot == 0)
+				quot = 1;
+		} else {
+			/* High-speed mode 3 */
+			uart_setreg(bas, MT_UART_HIGHS, 0x3);
+			uart_barrier(bas);
+
+			quot = (bas->rclk + 256 * baudrate - 1) /
+			    (256 * baudrate);
+			if (quot == 0)
+				quot = 1;
+		}
+
+		/* Program divisor latch */
+		uart_setreg(bas, REG_LCR, lcr | CFCR_DLAB);
+		uart_barrier(bas);
+
+		uart_setreg(bas, REG_DATA, quot & 0xFF);
+		uart_setreg(bas, REG_IER, (quot >> 8) & 0xFF);
+		uart_barrier(bas);
+
+		uart_setreg(bas, REG_LCR, lcr);
+		uart_barrier(bas);
+
+		/* Program sample count/point and fractional divisor */
+		if (baudrate >= 115200) {
+			tmp = (bas->rclk / (baudrate * quot)) - 1;
+			uart_setreg(bas, MT_UART_SAMPLE_COUNT, tmp);
+			uart_setreg(bas, MT_UART_SAMPLE_POINT, (tmp >> 1) - 1);
+			uart_barrier(bas);
+
+			fraction = ((bas->rclk * 100) / baudrate / quot) % 100;
+			fraction = (fraction + 5) / 10;
+			if (fraction > 10)
+				fraction = 10;
+
+			uart_setreg(bas, MT_UART_FRACDIV_L,
+			    mt_fraction_L_mapping[fraction]);
+			uart_setreg(bas, MT_UART_FRACDIV_M,
+			    mt_fraction_M_mapping[fraction]);
+		} else {
+			uart_setreg(bas, MT_UART_SAMPLE_COUNT, 0x00);
+			uart_setreg(bas, MT_UART_SAMPLE_POINT, 0xFF);
+			uart_setreg(bas, MT_UART_FRACDIV_L, 0x00);
+			uart_setreg(bas, MT_UART_FRACDIV_M, 0x00);
+		}
+		uart_barrier(bas);
+	}
+
 	uart_unlock(sc->sc_hwmtx);
+
+	return (0);
 }
 
 static kobj_method_t mdtk_methods[] = {
@@ -130,7 +247,7 @@ static kobj_method_t mdtk_methods[] = {
 	KOBJMETHOD(uart_getsig,		ns8250_bus_getsig),
 	KOBJMETHOD(uart_ioctl,		ns8250_bus_ioctl),
 	KOBJMETHOD(uart_ipend,		ns8250_bus_ipend),
-	KOBJMETHOD(uart_param,		ns8250_bus_param),
+	KOBJMETHOD(uart_param,		mt_bus_param),
 	KOBJMETHOD(uart_receive,	ns8250_bus_receive),
 	KOBJMETHOD(uart_setsig,		ns8250_bus_setsig),
 	KOBJMETHOD(uart_transmit,	ns8250_bus_transmit),
